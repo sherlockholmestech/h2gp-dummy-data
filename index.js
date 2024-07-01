acf10ccab4f2cd3da13f675e011d8dc3eea5f1ba
Sherlock Holmes <sherlockholmesxp@gmail.com>
2024-07-01 21:23:28 +0800

    changes

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const port = 8081
 
-app.get('/', (req, res) => {
+app.get('/volatge/battery', (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	let send_number = 7 + Math.random();
 	res.send(send_number.toString());
 })
+
+app.get('/amperage/battery', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let send_number = 5.7 + Math.random();
+	res.send(send_number.toString());
+})
+
+app.get('/temperature', (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let send_number = 25 + Math.random()*5;
+	res.send(send_number.toString());
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
